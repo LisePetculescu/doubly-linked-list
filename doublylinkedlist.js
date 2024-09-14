f3d2c8df1ec126d38fbdd9b6df49e22b2d135aa6
Lise Freja Jensen Petculescu <124042840+LisePetculescu@users.noreply.github.com>
2024-09-14 15:48:56 +0200

    færdig (i hvert fald mentalt)

--- doublylinkedlist.js
+++ doublylinkedlist.js
@@ -23,31 +23,72 @@ export default class DoublyLinkedList {
   }
 
   // - `get( index )` - returnerer elementet på plads nummer *index*
-  get(index) {}
+  get(index) {
+    const node = this.nodeAt(index);
+    return node ? node.data : null;
+  }
 
   // - `indexOf( data )` - finder plads nummer for det angivne element (payload)
-  indexOf(data) {}
+  indexOf(data) {
+    let current = this.head;
+    let index = 0;
+    while (current) {
+      if (current.data === data) return index;
+      current = current.next;
+      index++;
+    }
+    return -1;
+  }
 
   // - `insertAfter( index, data )` - indsætter et nyt element efter plads nummer *index*
-  insertAfter(index, data) {}
+  insertAfter(index, data) {
+    const existingNode = this.nodeAt(index);
+    if (!existingNode) return;
+    const newNode = new Node(data);
+    this.insertAfterNode(newNode, existingNode);
+  }
 
   // - `insertBefore( index, data )` - indsætter et nyt element før plads nummer *index*
-  insertBefore(index, data) {}
+  insertBefore(index, data) {
+    const existingNode = this.nodeAt(index);
+    if (!existingNode) return;
+    const newNode = new Node(data);
+    this.insertBeforeNode(newNode, existingNode);
+  }
 
   // - `first()` - returnerer det første element i listen
-  first() {}
+  first() {
+    return this.head ? this.head.data : null;
+  }
 
   // - `last()` - returnerer det sidste element i listen
-  last() {}
+  last() {
+    return this.tail ? this.tail.data : null;
+  }
 
   // - `remove( data )` - fjerner elementet fra listen (hvis det altså var der)
-  remove(data) {}
+  remove(data) {
+    let current = this.head;
+    while (current) {
+      if (current.data === data) {
+        this.removeNode(current);
+        return true;
+      }
+      current = current.next;
+    }
+    return false;
+  }
 
   // - `removeIndex( index )` - fjerner elementet på det pågældende *index*
-  removeIndex(index) {}
+  removeIndex(index) {
+    const node = this.nodeAt(index);
+    if (node) this.removeNode(node);
+  }
 
   // - `removeFirst()` - fjerner det første element i listen - og returnerer elementet
   removeFirst() {
+    if (!this.head) return null;
+
     const oldHead = this.head;
 
     if (oldHead?.next) {
@@ -61,6 +102,8 @@ export default class DoublyLinkedList {
 
   // - `removeLast()` - fjerner det sidste element i listen - og returnerer elementet
   removeLast() {
+    if (!this.tail) return null;
+
     const oldTail = this.tail;
 
     if (oldTail?.prev) {
@@ -125,6 +168,9 @@ export default class DoublyLinkedList {
 
   // - `removeNode( existingNode )` - fjerner en eksisterende node fra listen
   removeNode(existingNode) {
+    if (!existingNode) return console.error("can't remove node: node doesn't exist");
+    ;
+
     if (existingNode === this.head) {
       this.removeFirst();
     } else if (existingNode === this.tail) {
@@ -132,16 +178,85 @@ export default class DoublyLinkedList {
     } else {
       const oldPrev = existingNode.prev;
       const oldNext = existingNode.next;
-      oldNext.prev = existingNode.prev;
-      oldPrev.next = existingNode.next;
+
+      if (oldPrev) oldPrev.next = oldNext;
+      if (oldNext) oldNext.prev = oldPrev;
     }
   }
 
   // - `nodeAt( index )` - returnerer noden på plads nummer *index*
-  nodeAt(index) {}
+  nodeAt(index) {
+    let current = this.head;
+    let i = 0;
+
+    while (current && i < index) {
+      current = current.next;
+      i++;
+    }
+    return current;
+  }
 
   // - `swapNodes( nodeA, nodeB )` - bytter om på to nodes pladser i listen
-  swapNodes(nodeA, nodeB) {}
+  // swapNodes(nodeA, nodeB) {
+  //   if (nodeA === nodeB) return;
+
+  //   const nodeAdata = nodeA.data;
+  //   nodeA.data = nodeB.data;
+  //   nodeB.data = nodeAdata;
+  // }
+
+  swapNodes(nodeA, nodeB) {
+    // If nodeA and nodeB are the same
+    if (nodeA === nodeB) return;
+
+    const prevA = nodeA.prev;
+    const nextA = nodeA.next;
+    const prevB = nodeB.prev;
+    const nextB = nodeB.next;
+
+    // if nodes are next to each other
+    if (nodeA.next === nodeB) {
+      // if nodeA is just before nodeB
+      nodeA.next = nextB;
+      nodeA.prev = nodeB;
+      nodeB.next = nodeA;
+      nodeB.prev = prevA;
+      if (nextB) nextB.prev = nodeA;
+      if (prevA) prevA.next = nodeB;
+    } else if (nodeB.next === nodeA) {
+      // if nodeB is just before nodeA
+      nodeB.next = nextA;
+      nodeB.prev = nodeA;
+      nodeA.next = nodeB;
+      nodeA.prev = prevB;
+      if (nextA) nextA.prev = nodeB;
+      if (prevB) prevB.next = nodeA;
+    } else {
+      // if the nodes are not next to each other
+      if (prevA) prevA.next = nodeB;
+      if (nextA) nextA.prev = nodeB;
+      if (prevB) prevB.next = nodeA;
+      if (nextB) nextB.prev = nodeA;
+
+      nodeA.next = nextB;
+      nodeA.prev = prevB;
+      nodeB.next = nextA;
+      nodeB.prev = prevA;
+    }
+
+    // if any of the nodes are head or tail
+    if (this.head === nodeA) {
+      this.head = nodeB;
+    } else if (this.head === nodeB) {
+      this.head = nodeA;
+    }
+
+    if (this.tail === nodeA) {
+      this.tail = nodeB;
+    } else if (this.tail === nodeB) {
+      this.tail = nodeA;
+    }
+  }
 
   // ******************** metoder til hele listen ***********************
   // - `clear()` - fjerner alle elementer fra listen
@@ -163,12 +278,18 @@ export default class DoublyLinkedList {
 
   // - `dumpList( )` - udskriver hele listen i console
   dumpList() {
+    if (!this.head) {
+      console.log("The list is empty.");
+      return;
+    }
+
     console.log(`
                 Linked list
             ====================
             head: ${this.head.data}
             tail: ${this.tail.data}
             `);
+
     let current = this.head;
     while (current) {
       console.log(`
--- test_doublylinkedlist.js
+++ test_doublylinkedlist.js
@@ -3,12 +3,23 @@ import DoublyLinkedList, { Node } from "./doublylinkedlist.js";
 window.addEventListener("load", start);
 
 function start() {
-  console.log("test is running");
+  console.log("Test is running");
 
   const nodes = createNodes();
-  testAddNode(nodes);
-  testAddData(nodes);
 
+  testAddNode(nodes);
+
+  testAddData();
+
+  testRemoveAndIndexOf();
+  testInsertAfterAndBefore();
+  testSwapNodes();
+  testRemoveIndex();
+  testGet();
+  testFirstAndLast();
+  testRemoveFirstAndLast();
+  testClear();
+  testSize();
 }
 
 function createNodes() {
@@ -17,47 +28,179 @@ function createNodes() {
   const node3 = new Node("R");
   const node4 = new Node("T");
   const node5 = new Node("S");
+
   node1.next = node2;
-  node2.next = node4;
   node2.prev = node1;
-  node4.prev = node2;
+  node2.next = node3;
+  node3.prev = node2;
+  node3.next = node4;
+  node4.prev = node3;
+  node4.next = node5;
+  node5.prev = node4;
 
   return { node1, node2, node3, node4, node5 };
 }
 
-function testAddNode({ node2, node1, node4 }) {
+function testAddNode(nodes) {
   const list = new DoublyLinkedList();
-  list.addNodeFirst(node2);
-  list.addNodeFirst(node1);
-  list.addNodeLast(node4);
-  console.log("List 1: addNode");
+  list.addNodeFirst(nodes.node2);
+  list.addNodeFirst(nodes.node1);
+  list.addNodeLast(nodes.node4);
+  console.log("List 1: Add Node");
   list.dumpList();
 }
 
 function testAddData() {
-  const list2 = new DoublyLinkedList();
-  list2.addFirst("a");
-  list2.addFirst("c");
-  list2.addLast("r");
-  console.log("List 2: addData");
-  list2.dumpList();
-  const list3 = new DoublyLinkedList();
-  list3.addLast("C");
-  list3.addLast("A");
-  list3.addLast("T");
-  console.log("List 3: addData");
-  list3.dumpList();
-  const list4 = new DoublyLinkedList();
-  list4.addFirst("T");
-  list4.addFirst("A");
-  list4.addFirst("C");
-  console.log("List 4: addData");
-  list4.dumpList();
-  testRemove(list4);
-  list4.dumpList();
-
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 2: Add Data");
+  list.dumpList();
 }
 
-function testRemove(list) {
-  list.addNodeLast(list.removeLast());
+function testRemoveAndIndexOf() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 3: Before Removal");
+  list.dumpList();
+
+  console.log("Removing 'A':", list.remove("A"));
+  console.log("List 3: After Removal");
+  list.dumpList();
+
+  console.log("Index of 'C':", list.indexOf("C"));
+  console.log("Index of 'S':", list.indexOf("S"));
+  console.log("Index of 'A':", list.indexOf("A"));
+}
+
+function testInsertAfterAndBefore() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 4: Before Insertions");
+  list.dumpList();
+
+  list.insertAfter(2, "X"); // adding "X" after index 2 --> "R"
+  console.log("List 4: After Insert 'X' After Index 2");
+  list.dumpList();
+
+  list.insertBefore(3, "Y"); // adding "Y" before index 3 --> "T"
+  console.log("List 4: After Insert 'Y' Before Index 3");
+  list.dumpList();
+}
+
+function testSwapNodes() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 5: Before Swap");
+  list.dumpList();
+
+  const nodeA = list.nodeAt(1);
+  const nodeB = list.nodeAt(3);
+  list.swapNodes(nodeA, nodeB);
+  console.log("List 5: After Swapping 'A' and 'T'");
+  list.dumpList();
+}
+
+function testRemoveIndex() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 6: Before Removal by Index");
+  list.dumpList();
+
+  list.removeIndex(2);
+  console.log("List 6: After Removal by Index 2");
+  list.dumpList();
+}
+
+function testGet() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 7: Testing `get()`");
+  console.log("Element at index 0:", list.get(0));
+  console.log("Element at index 2:", list.get(2));
+  console.log("Element at index 4:", list.get(4));
+  console.log("Element at index 5:", list.get(5)); // <-- should be null
+}
+
+function testFirstAndLast() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 8: Testing `first()` and `last()`");
+  console.log("First element:", list.first());
+  console.log("Last element:", list.last());
+}
+
+function testRemoveFirstAndLast() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 9: Before Removal of First and Last");
+  list.dumpList();
+
+  console.log("Removing first element:", list.removeFirst().data);
+  console.log("Removing last element:", list.removeLast().data);
+
+  console.log("List 9: After Removal of First and Last");
+  list.dumpList();
+}
+
+function testClear() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 10: Before Clear");
+  list.dumpList();
+
+  list.clear();
+
+  console.log("List 10: After Clear");
+  list.dumpList();
+}
+
+
+function testSize() {
+  const list = new DoublyLinkedList();
+  list.addLast("C");
+  list.addLast("A");
+  list.addLast("R");
+  list.addLast("T");
+  list.addLast("S");
+  console.log("List 11: Testing `size()`");
+  console.log("Size of list:", list.size());
+
+  list.removeLast();
+  console.log("Size of list after removeLast:", list.size());
 }
